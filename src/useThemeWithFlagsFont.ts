1f5fd802aa7c115d027681d2db9df5af7700549e
Arafat Zahan <arafat.zahan@10up.com>
2024-07-13 11:29:22 +0600

    Update hook.

--- src/useThemeWithFlagsFont.ts
+++ src/useThemeWithFlagsFont.ts
@@ -1,16 +1,24 @@
 import { useTheme } from 'react-native-paper';
+import type { MD2Theme, MD3Theme } from 'react-native-paper';
 import type { InternalTheme, ThemeProp } from 'react-native-paper/lib/typescript/types';
 import { defaultFlagsFont, isWeb } from './constants';
+import type { $DeepPartial } from './types';
 
 export default function useThemeWithFlagsFont(themeOverrides?: ThemeProp) {
   const theme = useTheme<InternalTheme>(themeOverrides);
 
   if (isWeb) {
     if (theme.isV3) {
-      theme.fonts.bodyLarge.fontFamily = `${defaultFlagsFont}, ${theme.fonts.bodyLarge.fontFamily}`;
-      theme.fonts.default.fontFamily = `${defaultFlagsFont}, ${theme.fonts.default.fontFamily}`;
+      if ((themeOverrides as $DeepPartial<MD3Theme>)?.fonts?.bodyLarge?.fontFamily === undefined) {
+        theme.fonts.bodyLarge.fontFamily = `${defaultFlagsFont}, ${theme.fonts.bodyLarge.fontFamily}`;
+      }
+      if ((themeOverrides as $DeepPartial<MD3Theme>)?.fonts?.default?.fontFamily === undefined) {
+        theme.fonts.default.fontFamily = `${defaultFlagsFont}, ${theme.fonts.default.fontFamily}`;
+      }
     } else {
-      theme.fonts.regular.fontFamily = `${defaultFlagsFont}, ${theme.fonts.regular.fontFamily}`;
+      if ((themeOverrides as $DeepPartial<MD2Theme>)?.fonts?.regular?.fontFamily === undefined) {
+        theme.fonts.regular.fontFamily = `${defaultFlagsFont}, ${theme.fonts.regular.fontFamily}`;
+      }
     }
   }
 
